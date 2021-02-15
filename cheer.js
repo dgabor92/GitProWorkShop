587bbdc7116e2170c1948e8793e17474192e9b2f
Martin Smitnya <smitnyamartin@gmail.com>
2021-02-15 10:25:10 +0100

    b1

--- cheer.js
+++ cheer.js
@@ -1,8 +1,12 @@
 'use strict';
 
-let teamName = 'Lila Pockok';
-Cheer(teamName);
+// let teamName = 'Lila Pockok';
+// Cheer(teamName);
+
+let choosenOne = 'JavaScript';
+Cheer(choosenOne);
 
 function Cheer(input) {
     console.log(input);
+
 }
