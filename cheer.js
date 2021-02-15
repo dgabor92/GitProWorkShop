e928dfc0dbac41a8e9fe15c984c95012c54a4e0a
MSCardo <vcsneu@gmail.com>
2021-02-15 10:39:31 +0100

    c5

--- cheer.js
+++ cheer.js
@@ -6,13 +6,4 @@ Cheer(teamName, ' a legjobbak!');
 
 function Cheer(input1, input2) {
     console.log(input1 + input2);
-
-    // let teamName = 'Lila Pockok';
-    // Cheer(teamName);
-
-    let choosenOne = 'JavaScript';
-    Cheer(choosenOne);
-
-    function Cheer(input) {
-        console.log(input);
-    }
+}
