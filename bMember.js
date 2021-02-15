2632e44bae4e9da1f8de28a93a0ca76573eebb9e
Martin Smitnya <smitnyamartin@gmail.com>
2021-02-15 10:34:14 +0100

    b5

--- bMember.js
+++ bMember.js
@@ -1,8 +1,9 @@
 'use strict';
 
 let month = 11;
+let age = 26;
 function count() {
-  console.log('I born in ' + month);
+  console.log('I born in ' + age + ' at '+ month +'th. month');
 }
 
 count();
